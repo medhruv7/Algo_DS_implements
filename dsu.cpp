524b693255791784d588c8c7e31714a12da09074
dhruv <medhruv7@gmail.com>
2020-06-27 00:05:33 +0530

    DSU Code

---
+++ dsu.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> par,sz;
+
+int getPar(int n){
+    return (n == par[n] ? n : par[n] = getPar(par[n]));
+}
+
+void unite(int x,int y){
+    x = getPar(x);
+    y = getPar(y);
+    if(x == y) return;
+    if(sz[x] < sz[y]) swap(x,y);
+    par[y] = x;
+    sz[y] += sz[x];
+    sz[x] = 1;
+    return;
+}
+
+void init(int n){
+    for(int i = 0; i < n; ++i){
+        par[i] = i;
+        sz[i] = 1;
+    }
+}
